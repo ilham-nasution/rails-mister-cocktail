9af13019dcc1b1d38f1885a7633437c2d0d27e00
Ilham Rahman <ilhaamrahman@gmail.com>
2019-11-07 17:34:44 +0900

    create a cocktail page

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,37 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to edit_cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
