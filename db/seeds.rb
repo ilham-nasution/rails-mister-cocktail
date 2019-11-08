5bd54679b6d1cbd8f86ddc05d1d8c52c883afd84
Ilham Rahman <ilhaamrahman@gmail.com>
2019-11-08 15:20:03 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 require 'faker'
+require 'json'
+require 'open-uri'
 
-puts "creating...."
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data = open(url).read
+ingredient = JSON.parse(data)
+
+puts "creating ingredients...."
+ingredient["drinks"].each do |x|
+  Ingredient.create!(name: x["strIngredient1"])
+end
+puts "... ingredients finished"
+
+puts "creating cocktails...."
 20.times do
   Cocktail.create!(name: Faker::Coffee.blend_name)
 end
-puts "...finished"
+puts "...cocktails finished"
